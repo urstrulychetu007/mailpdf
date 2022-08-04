04e92b4c0f9555600bd4a534f8aaaa463f09839c
urstrulychetu007 <60094500+urstrulychetu007@users.noreply.github.com>
2022-08-04 19:36:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ mail = Mail(app)
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
 app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = 'kletechseed_data@kletech.ac.in'
-app.config['MAIL_PASSWORD'] = 'kletechseed_data'
+app.config['MAIL_USERNAME'] = 'kletech_data@kletech.ac.in'
+app.config['MAIL_PASSWORD'] = 'kletech_data'
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 mail = Mail(app)
